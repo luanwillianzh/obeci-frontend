358388c9dd235230067ddb83a016dc51ea18f5b9
sandro <sandrorobertojunior@hotmail.com>
2025-10-21 22:58:05 -0300

    criação das pastas de organização

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import "./loginpage.css";
+export default function Login() {
+  return (
+    <>
+      <h1 className="h1">Login Page</h1>
+    </>
+  );
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+"use client";
+import "./header.css";
+export default function Header() {
+  return (
+    <header className="header-obeci">
+      <div className="container-imagem-obeci">
+        <img src="/logo-obeci.png" alt="Logo do projeto OBECI" />
+      </div>
+      <nav className="nav-vazia"></nav>
+    </header>
+  );
+}
