30b4896cbd67b2a5d59c6c95a3aa1b2d353b85e0
luanwillianzh <luanwillianzh@outlook.com>
2025-10-31 09:56:11 -0300

    Update Classes Page

--- src/app/turmas/page.tsx
+++ src/app/turmas/page.tsx
@@ -7,9 +7,35 @@ import "./turmas.css";
 export default function Turma() {
   return (
     <>
-    <Title text="Turmas"></Title>
-    <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
-    <ButtonObeci text="Adicionar Turma"></ButtonObeci>
+    <div style={{padding: '2.5rem'}}>
+      <div style={{display: 'grid', gridTemplateColumns: '5fr 2fr', justifyContent: 'center', gap: '2rem'}}>
+        <div>
+          <Title text="Turmas"></Title>
+          <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly', gap: '5rem, 5rem', marginBottom: '1rem'}}>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+            <ClassCard class_name="Turma A" turno="Manhã" q_alunos={30} class_id="1"></ClassCard>
+          </div>
+        </div>
+        <div className="lembrete">
+        </div>
+      </div>
+    </div>
     </>
   );
 }
